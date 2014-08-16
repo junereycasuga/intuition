388828bd2ca5853cfc9cf02adc4e6bccd400cdfb
Junerey Casuga <junerey.casuga@gmail.com>
2014-08-16 18:40:46 +0800

    DB Update

--- protected/DB/intuition.sql
+++ protected/DB/intuition.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 16, 2014 at 04:24 PM
+-- Generation Time: Aug 16, 2014 at 06:40 PM
 -- Server version: 5.5.38-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.3
 
@@ -52,7 +52,7 @@ CREATE TABLE IF NOT EXISTS `created_insights` (
 --
 
 CREATE TABLE IF NOT EXISTS `users` (
-  `id` varchar(10) NOT NULL,
+  `id` varchar(20) NOT NULL,
   `username` varchar(100) NOT NULL,
   `user_email` varchar(100) NOT NULL,
   `user_password` text NOT NULL,
@@ -60,6 +60,13 @@ CREATE TABLE IF NOT EXISTS `users` (
   `user_lastname` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `user_email`, `user_password`, `user_firstname`, `user_lastname`) VALUES
+('53ef32ecda3b7', 'junereycasuga', 'junerey.casuga@gmail.com', '8b50e8e5196f4e01c6856bc3910d60c6c3b1e119bc0b2fbc026441bc8927e0ec822754907a29e27f3d027230c5a9465f00f1f2526fa177d3eeef334d66bf037e', 'Junerey', 'Casuga');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
